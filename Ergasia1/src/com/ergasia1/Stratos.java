4b4d2fd340c97c2bcf7d6eeb171bafb47d60a50f
Stratos K <stratoskarkanis2@gmail.com>
2021-03-23 18:44:39 +0200

    New classsssssssss

---
+++ Ergasia1/src/com/ergasia1/Stratos.java
@@ -0,0 +1,13 @@
+package com.ergasia1;
+
+public class Stratos {
+
+    static int age = 120;
+    static String name = "Stratos";
+
+    public static void main (String[] args){
+        System.out.println(age);
+        System.out.println(name);
+    }
+
+}
