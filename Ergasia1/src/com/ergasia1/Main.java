9fa60b8fe06c6d243eb684e0eebdbacbd48f93a3
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-03-23 18:07:46 +0200

    added stratos line

--- Ergasia1/src/com/ergasia1/Main.java
+++ Ergasia1/src/com/ergasia1/Main.java
@@ -7,5 +7,7 @@ public class Main
         Testing test = new Testing("Giorgos", 19);
         System.out.println(test.getAge());
         System.out.println(test.getName());
+
+        String a1 = "Stratos";
     }
 }
