79a846d969f1913dfb9ee970fd6ddc6fd3dcb3bf
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-03-31 21:25:45 +0300

    Δοκιμές της Κλάσης Patient

--- Ergasia1/src/com/ergasia1/CreateUsers.java
+++ Ergasia1/src/com/ergasia1/CreateUsers.java
@@ -1,9 +1,17 @@
 package com.ergasia1;
 
-public class Main
+import java.util.Scanner;
+
+public class CreateUsers
 {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args)
     {
         System.out.println("Starting.");
+
+        Patient p1 = new Patient("GeorgeMC2610", "blabla", "555465652");
+
+        p1.Login("GeorgeMC2610", "blabla");
     }
 }
