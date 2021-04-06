e94e50ba5f5cdd0524ff44e105a3713bf937e89c
Stratos K <stratoskarkanis2@gmail.com>
2021-04-06 19:24:15 +0300

    Έφτιαξα την κλάση appointment. Έβαλα δύο νέες μεθόδους.

--- Ergasia1/src/com/ergasia1/Appointment.java
+++ Ergasia1/src/com/ergasia1/Appointment.java
@@ -1,7 +1,8 @@
 package com.ergasia1;
 
 /**
- * This class is used for managing appointments
+ * This class is used for managing appointments (insert,delete and modify an appointment)
+ * The appointments will be saved to the database
  */
 public class Appointment
 {
@@ -13,6 +14,26 @@ public class Appointment
     void insertNewAppointment(){
 
         System.out.println("Appointment was added to the database!");
+
+    }
+
+    /**
+     * This function is used for deleting an existing
+     * appointment from the database
+     */
+    void deleteAppointment(){
+
+        System.out.println("Appointment was deleted from the database!");
+
+    }
+
+    /**
+     * This function is used for modifying an existing
+     * appointment from the database
+     */
+    void modifyAppointment(){
+
+        System.out.println("Appointment was successfully modified from the database!");
 
     }
 
