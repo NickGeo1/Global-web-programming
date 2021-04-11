12220c57083c4d027dae60631c37ffbf742e6307
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-04-11 11:54:42 +0300

    Το path είναι, πλέον, σχετικό!

--- Ergasia1/src/com/ergasia1/UserInteraction.java
+++ Ergasia1/src/com/ergasia1/UserInteraction.java
@@ -262,7 +262,7 @@ public class UserInteraction {
 
         try
         {
-            outputStream = new BufferedWriter ( new FileWriter("C:\\Users\\nikos\\Documents\\GitHub\\Global-web-programming\\Ergasia1\\src\\com\\ergasia1\\Patient_attr",true) );
+            outputStream = new BufferedWriter ( new FileWriter("src/com/ergasia1/Patient_attr",true) );
             outputStream.write("\n"+p2.getUsername() +","+ p2.getPassword() +","+ p2.getFirstname() +","+ p2.getSurname() +","+ p2.getAge() +","+ p2.GetAMKA());
             System.out.println("Successfully stored above patient in output file!");
             TimeUnit.SECONDS.sleep(2);
@@ -295,7 +295,7 @@ public class UserInteraction {
 
         try
         {
-            inputStream = new BufferedReader ( new FileReader("C:\\Users\\nikos\\Documents\\GitHub\\Global-web-programming\\Ergasia1\\src\\com\\ergasia1\\Patient_attr") );
+            inputStream = new BufferedReader ( new FileReader("src/com/ergasia1/Patient_attr") );
             String[] attr = inputStream.readLine().split(",");
 
             if(attr.length>6)
