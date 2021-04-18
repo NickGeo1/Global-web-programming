5c02ca2108325c87a01c32b8c7524122e3a021fe
Stratos K <stratoskarkanis2@gmail.com>
2021-04-18 14:06:35 +0300

    Έβαλα την εκφώνηση της άσκησης και έκανα κάτι αλλαγές στις μορφοποιήσεις.

--- Ergasia1/src/com/ergasia1/UserInteraction.java
+++ Ergasia1/src/com/ergasia1/UserInteraction.java
@@ -77,7 +77,7 @@ public class UserInteraction {
                     TimeUnit.SECONDS.sleep(3);
 
                     System.out.println("[DOCTOR OBJECT]: Attempting to log in " + d1.getUsername() + ".");
-                    d1.Login("iamadoctor");
+                    d1.Login("iamadocotr");
 
                     TimeUnit.SECONDS.sleep(2);
 
