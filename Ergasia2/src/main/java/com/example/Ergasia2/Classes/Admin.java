274e0996c290f8c46d90fabf1ea120ea4f44d7bf
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-05-28 17:51:51 +0300

    προσθήκη κλάσεων εντός του project

---
+++ Ergasia2/src/main/java/com/example/Ergasia2/Classes/Admin.java
@@ -0,0 +1,71 @@
+package com.example.Ergasia2.Classes;
+
+/**
+ * This is the model of an Admin. Admins are the only ones who can add,modify and delete doctors from the
+ * application
+ */
+public class Admin extends Users
+{
+    // Constructor method
+    public Admin(String username, String password, String firstname, String surname, int age) {
+
+        super(username, password, firstname, surname, age);
+
+    }
+
+    /**
+     * @param doctor is a type of User. So, the function takes an object of type User and destroyes it.
+     * This function is used to delete a doctor that already exists.
+     */
+    public void delete_doctor(Users doctor){
+
+        if (!isLoggedOn())
+        {
+
+            System.out.println("You must be logged on to delete a doctor.");
+            return;
+        }
+
+        System.out.println("Doctor " + doctor.getUsername() + " has been deleted!");
+    }
+
+    /**
+     * This function is used to add/create a new doctor.
+     * @return an object of type Doctor
+     */
+    public Doctor add_doctor(String username, String password, String firstname, String surname, int age,String speciality){
+
+        if (!isLoggedOn())
+        {
+            System.out.println("You must be logged on to add a new Doctor.");
+            return null;
+        }
+
+        System.out.println("New doctor added!");
+        return new Doctor (username,password,firstname,surname,age,speciality);
+    }
+
+    /**
+     * This function is used to modify a doctor that already exists.
+     * @param username is the username of the doctor that we want to modify
+     */
+    public void change_doctor(String username){
+
+        if (!isLoggedOn())
+        {
+            System.out.println("You must be logged on to modify a doctor.");
+            return;
+        }
+
+        System.out.println("Doctor's characteristics have been changed!");
+    }
+
+    /**
+     * This method returns the characteristics of each Admin
+     * @return firstname,username,surname and age of Admin object
+     */
+    @Override
+    public String toString(){
+        return super.toString();
+    }
+}
---
+++ Ergasia2/src/main/java/com/example/Ergasia2/Classes/Appointment.java
@@ -0,0 +1,63 @@
+package com.example.Ergasia2.Classes;
+import java.util.Date;
+
+/**
+ * This class is used for managing appointments (insert,delete and modify an appointment)
+ * The appointments will be saved to the database
+ */
+public class Appointment
+{
+
+    Date date; // The date of the appointment
+    Boolean done = false;
+
+    // Constructor method
+    Appointment (Date d,Boolean done){
+
+        this.done = true;
+        this.date = d;
+    }
+
+    /**
+     * This function is used for inserting a new
+     * appointment to the database
+     */
+    void insertNewAppointment(){
+
+        if (done)
+            System.out.println("Appointment was added to the database!");
+
+    }
+
+    /**
+     * This function is used for deleting an existing
+     * appointment from the database
+     */
+    void deleteAppointment(){
+
+        if (done)
+            System.out.println("Appointment was deleted from the database!");
+
+    }
+
+    /**
+     * This function is used for modifying an existing
+     * appointment from the database
+     */
+    void modifyAppointment(){
+
+        if (done)
+            System.out.println("Appointment was successfully modified!");
+
+    }
+
+    /**
+     * This function is used for desplaying the
+     *  date and status of an appointment.
+     */
+    @Override
+    public String toString(){
+        return  "Date: "+date +", is done: "+done;
+    }
+
+}
