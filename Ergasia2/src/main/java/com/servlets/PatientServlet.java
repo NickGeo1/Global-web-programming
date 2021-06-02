dd16079bee414f848716b99bfe3f147cbf397d1b
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-06-02 19:36:34 +0300

    anebasma vashs

--- Ergasia2/src/main/java/com/servlets/PatientServlet.java
+++ Ergasia2/src/main/java/com/servlets/PatientServlet.java
@@ -1,12 +1,28 @@
 package com.servlets;
 
 import java.io.*;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sql.DataSource;
 
 @WebServlet(name = "PatientServlet", value = "/patientlogin")
 public class PatientServlet extends HttpServlet
 {
+    private DataSource datasource = null;
+
+    public void init() throws ServletException {
+        try {
+
+            InitialContext ctx = new InitialContext();
+            datasource = (DataSource)ctx.lookup("java:comp/env/jdbc/LiveDataSource");
+
+        } catch(Exception e) {
+            throw new ServletException(e.toString());
+        }
+
+    }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
        String pat =  request.getParameter("patient");
