2639ba9ff49a261182c624778f97101d3a74d4cc
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-02 13:13:07 +0300

    added a dummy new servlet for registering a patient

---
+++ Ergasia2/src/main/java/com/servlets/PatientRegisterServlet.java
@@ -0,0 +1,26 @@
+package com.servlets;
+
+import com.classes.Patient;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PatientServlet", value = "/patientregister")
+public class PatientRegisterServlet extends HttpServlet
+{
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String firstname = request.getParameter("fn");
+        String lastname = request.getParameter("ln");
+        int age = Integer.parseInt(request.getParameter("age"));
+        String AMKA = request.getParameter("AMKA");
+
+        Patient patient = new Patient(username, password, firstname, lastname, age, AMKA);
+        System.out.println(patient.toString());
+    }
+}
