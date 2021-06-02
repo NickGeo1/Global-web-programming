51f2d35aacd59bfd4bf4d04fc59881ab54346d43
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-02 11:07:45 +0300

    remade the servlets

--- Ergasia2/src/main/java/com/servlets/PatientServlet.java
+++ Ergasia2/src/main/java/com/servlets/PatientServlet.java
@@ -4,7 +4,7 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "PatientServlet", value = "/login")
+@WebServlet(name = "PatientServlet", value = "/patientlogin")
 public class PatientServlet extends HttpServlet
 {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
